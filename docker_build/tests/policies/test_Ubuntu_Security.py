7365ddfe023cc08cd0f2c923996dd1887022e229
Dinis Cruz <dinis.cruz@owasp.org>
2019-09-13 11:09:44 +0100

    minor fixes

--- docker_build/tests/policies/test_Ubuntu_Security.py
+++ docker_build/tests/policies/test_Ubuntu_Security.py
@@ -1,20 +1,12 @@
 from unittest import TestCase
-
-from pbx_gs_python_utils.utils.Dev import Dev
-
 from docker_build.src.builds.Ubuntu_Build import Ubuntu_Build
 
 
 class test_Ubuntu_Security(TestCase):
-
+    
     def setUp(self):
+        # API that provides access to docker build of Ubuntu (note: each command below is executed on an clean container)
         self.ubuntu = Ubuntu_Build()
-        self.result = None
-
-
-    def tearDown(self):                 # to help unit test developmennt
-        if self.result is not None:
-            Dev.pprint(self.result)
 
     def test_POLICY_5__required_passwords_to_be_16_chars(self):
         # confirm that the password settings exists in the '/etc/pam.d/common-password' file
@@ -28,8 +20,11 @@ class test_Ubuntu_Security(TestCase):
 
 
     def test_POLICY_9__show_message_on_login(self):
+        # confirm that login message is in the correct location
         assert "This system is for the use of authorised" in self.ubuntu.file_etc_motd()
 
-
     def test_POLICY_22_anti_virus_is_installed(self):
+        # confirm that clamscan executes ok
         assert '----------- SCAN SUMMARY -----------' in self.ubuntu.run(['clamscan','–-help']).get('console')
+
+
